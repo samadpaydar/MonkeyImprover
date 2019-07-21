9e63d3c7e76f24f189272d066fb7e9f2e4cc3677
Samad Paydar <samadpaydar@gmail.com>
2019-07-21 12:03:26 +0430

    Improve code
    Implementation of the ClassFinder and CallbackFinder classes are modified.

--- src/ir/ac/um/monkeyimprover/analysis/layouts/callbacks/StaticCallbackFinder.java
+++ src/ir/ac/um/monkeyimprover/analysis/layouts/callbacks/StaticCallbackFinder.java
@@ -3,9 +3,11 @@ package ir.ac.um.monkeyimprover.analysis.layouts.callbacks;
 import com.intellij.openapi.vfs.VirtualFile;
 import ir.ac.um.monkeyimprover.analysis.MonkeyImprover;
 import ir.ac.um.monkeyimprover.analysis.classes.ClassFinder;
+import ir.ac.um.monkeyimprover.analysis.layouts.LayoutInformationExtractor;
 import ir.ac.um.monkeyimprover.analysis.layouts.callbacks.CallbackFinder;
 import ir.ac.um.monkeyimprover.analysis.methods.CallbackMethodInfo;
 import ir.ac.um.monkeyimprover.analysis.methods.MethodComplexityAnalyzer;
+import ir.ac.um.monkeyimprover.utils.Utils;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -34,8 +36,14 @@ public class StaticCallbackFinder extends CallbackFinder {
         MethodComplexityAnalyzer methodComplexityAnalyzer = new MethodComplexityAnalyzer(monkeyImprover);
         if (callbackMethodNames != null && !callbackMethodNames.isEmpty()) {
             ClassFinder classFinder = new ClassFinder(monkeyImprover);
-            List<VirtualFile> relatedJavaFiles = classFinder.findRelatedJavaFile(projectBaseDirectory, layoutFile, null);
-            if (relatedJavaFiles != null && !relatedJavaFiles.isEmpty()) {
+            List<VirtualFile> allJavaFiles = classFinder.getAllJavaFiles(projectBaseDirectory);
+            List<VirtualFile> relatedJavaFiles = new ArrayList<>();
+            for (VirtualFile javaFile : allJavaFiles) {
+                if (isRelated(javaFile, layoutFile)) {
+                    relatedJavaFiles.add(javaFile);
+                }
+            }
+            if (!relatedJavaFiles.isEmpty()) {
                 for (String callbackMethodName : callbackMethodNames) {
                     CallbackMethodInfo info = methodComplexityAnalyzer.getCallbackMethodInfo(callbackMethodName, relatedJavaFiles);
                     infoList.add(info);
@@ -44,6 +52,71 @@ public class StaticCallbackFinder extends CallbackFinder {
         }
         return infoList;
     }
+
+    private boolean isRelated(VirtualFile javaFile, VirtualFile layoutXMLFile) {
+        return isRelatedByContext(javaFile, layoutXMLFile) || isRelatedByName(javaFile, layoutXMLFile);
+    }
+
+    private boolean isRelatedByContext(VirtualFile javaFile, VirtualFile layoutXMLFile) {
+        boolean result = false;
+        LayoutInformationExtractor layoutInformationExtractor = new LayoutInformationExtractor(monkeyImprover);
+        File xmlFile = new File(layoutXMLFile.getCanonicalPath());
+        List<String> contextClassNames = layoutInformationExtractor.getContextClassNames(xmlFile);
+        if (contextClassNames != null && !contextClassNames.isEmpty()) {
+            for (String contextClassName : contextClassNames) {
+                String childName = javaFile.getNameWithoutExtension();
+                String childPath = createParentHierarchy(javaFile);
+                String packageName = getPackageName(contextClassName);
+                String name = getName(contextClassName);
+                if (name != null && name.equals(childName)
+                        && packageName != null && childPath.endsWith(packageName)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean isRelatedByName(VirtualFile javaFile, VirtualFile layoutXMLFile) {
+        String layoutFileName = layoutXMLFile.getName();
+        layoutFileName = layoutFileName.substring(0, layoutFileName.lastIndexOf('.'));
+        String[] parts = layoutFileName.split("_");
+        String relatedJavaFileName = "";
+        for (String part : parts) {
+            relatedJavaFileName = Utils.capitalize(part) + relatedJavaFileName;
+        }
+        relatedJavaFileName += ".java";
+        return relatedJavaFileName.equals(javaFile.getName());
+    }
+
+    private String createParentHierarchy(VirtualFile child) {
+        VirtualFile parent = child.getParent();
+        if (parent != null) {
+            return createParentHierarchy(parent) + "." + parent.getName();
+        } else {
+            return "";
+        }
+    }
+
+    private String getName(String javaFileName) {
+        String name = null;
+        int index = javaFileName.lastIndexOf('.');
+        if (index != -1) {
+            name = javaFileName.substring(index + 1);
+        }
+        return name;
+    }
+
+
+    private String getPackageName(String javaFileName) {
+        String packageName = null;
+        int index = javaFileName.lastIndexOf('.');
+        if (index != -1) {
+            packageName = javaFileName.substring(0, index);
+        }
+        return packageName;
+    }
+
 
     private List<String> getCallbackMethodNames(File xmlFile) {
         if (xmlFile.exists() && xmlFile.isFile()) {
