f533ab1f6e14df6406c6f87492f5d1cad06fe7d9
Samad Paydar <samadpaydar@gmail.com>
2019-07-21 15:40:31 +0430

    Implement DynamicCallbackFinder

--- src/ir/ac/um/monkeyimprover/analysis/layouts/callbacks/DynamicCallbackFinder.java
+++ src/ir/ac/um/monkeyimprover/analysis/layouts/callbacks/DynamicCallbackFinder.java
@@ -37,15 +37,8 @@ public class DynamicCallbackFinder extends CallbackFinder {
             ClassFinder classFinder = new ClassFinder(monkeyImprover);
             List<VirtualFile> allJavaFiles = classFinder.getAllJavaFilesInSrcDirectory();
             for (String viewId : viewIds) {
-                List<VirtualFile> relatedJavaFiles = new ArrayList<>();
                 for (VirtualFile javaFile : allJavaFiles) {
-                    if (isRelated(javaFile, viewId)) {
-                        Utils.showMessage("JavaFile " + javaFile + " has a findViewById for " + viewId);
-                        relatedJavaFiles.add(javaFile);
-                    }
-                }
-                if (relatedJavaFiles != null && !relatedJavaFiles.isEmpty()) {
-                    CallbackMethodInfo info = getCallbackMethodInfoByViewId(viewId, relatedJavaFiles);
+                    CallbackMethodInfo info = getCallbackMethodInfo(javaFile, viewId);
                     if (info != null) {
                         infoList.add(info);
                     }
@@ -55,82 +48,15 @@ public class DynamicCallbackFinder extends CallbackFinder {
         return infoList;
     }
 
-    private boolean isRelated(VirtualFile javaFile, String viewId) {
-        final List<VirtualFile> relatedFiles = new ArrayList<>();
+    private CallbackMethodInfo getCallbackMethodInfo(VirtualFile javaFile, String viewId) {
+        final CallbackMethodInfo info = null;
         PsiFile psiFile = PsiManager.getInstance(monkeyImprover.getProject()).findFile(javaFile);
         if (psiFile instanceof PsiJavaFile) {
             PsiJavaFile psiJavaFile = (PsiJavaFile) psiFile;
-            psiJavaFile.accept(new JavaRecursiveElementVisitor() {
-                @Override
-                public void visitAssignmentExpression(PsiAssignmentExpression expression) {
-                    super.visitExpression(expression);
-                    if (setsOnClickListenerForView(expression, viewId)) {
-                        relatedFiles.add(javaFile);
-                    }
-                }
-
-                @Override
-                public void visitDeclarationStatement(PsiDeclarationStatement statement) {
-                    super.visitStatement(statement);
-                    PsiDeclarationStatementImpl declarationStatement = (PsiDeclarationStatementImpl) statement;
-                    try {
-                        PsiElement[] children = declarationStatement.getChildren();
-                        if (children.length > 0 && children[0] instanceof PsiLocalVariableImpl) {
-                            PsiLocalVariableImpl variable = (PsiLocalVariableImpl) children[0];
-                            PsiExpression expression = variable.getInitializer();
-                            if (expression instanceof PsiTypeCastExpressionImpl) {
-                                PsiTypeCastExpressionImpl typeCastExpression = (PsiTypeCastExpressionImpl) expression;
-                                PsiExpression operand = typeCastExpression.getOperand();
-                                if (operand instanceof PsiMethodCallExpressionImpl) {
-                                    if (setsOnClickListenerForView((PsiMethodCallExpression) operand, viewId)) {
-                                        Utils.showMessage("stored view in a variable named: " + variable.getText());
-                                        relatedFiles.add(javaFile);
-                                    }
-                                }
-                            }
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            });
+            DynamicCallbackVisitor visitor = new DynamicCallbackVisitor(viewId);
+            psiJavaFile.accept(visitor);
         }
-        return !relatedFiles.isEmpty();
-    }
-
-    private boolean setsOnClickListenerForView(PsiMethodCallExpression methodCallExpression, String viewId) {
-        boolean result = false;
-        String calledMethodName = methodCallExpression.getMethodExpression().getReferenceName();
-        if (calledMethodName.equals("findViewById")) {
-            PsiExpressionList arguments = methodCallExpression.getArgumentList();
-            PsiExpression firstArgument = arguments.getExpressions()[0];
-            if (firstArgument.getText().equals("R.id." + viewId)) {
-                result = true;
-            }
-        }
-
-        return result;
-    }
-
-    private boolean setsOnClickListenerForView(PsiAssignmentExpression assignmentExpression, String viewId) {
-        boolean result = false;
-        PsiExpression rightExpression = assignmentExpression.getRExpression();
-        PsiExpression leftExpression = assignmentExpression.getLExpression();
-        if (rightExpression instanceof PsiMethodCallExpression) {
-            PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) rightExpression;
-            String calledMethodName = methodCallExpression.getMethodExpression().getReferenceName();
-            if (calledMethodName.equals("findViewById")) {
-                PsiExpressionList arguments = methodCallExpression.getArgumentList();
-                PsiExpression firstArgument = arguments.getExpressions()[0];
-                if (firstArgument.getText().equals("R.id." + viewId)) {
-                    Utils.showMessage("stored view in a variable named: " + leftExpression.getText());
-                    result = true;
-                }
-            }
-        }
-
-        return result;
+        return info;
     }
 
     private CallbackMethodInfo getCallbackMethodInfoByViewId(String viewId, List<VirtualFile> relatedJavaFiles) {
@@ -152,4 +78,109 @@ public class DynamicCallbackFinder extends CallbackFinder {
         return info;
     }
 
+}
+
+
+class DynamicCallbackVisitor extends JavaRecursiveElementVisitor {
+    private List<DynamicCallBackInfo> dynamicCallBackInfos = new ArrayList<>();
+    private String viewId;
+
+    public DynamicCallbackVisitor(String viewId) {
+        this.viewId = viewId;
+    }
+
+    @Override
+    public void visitAssignmentExpression(PsiAssignmentExpression expression) {
+        super.visitExpression(expression);
+        DynamicCallBackInfo info = findDynamicCallbackInfoForView(expression, viewId);
+        dynamicCallBackInfos.add(info);
+    }
+
+    @Override
+    public void visitDeclarationStatement(PsiDeclarationStatement statement) {
+        super.visitStatement(statement);
+        DynamicCallBackInfo info = findDynamicCallbackInfoForView(statement, viewId);
+        dynamicCallBackInfos.add(info);
+    }
+
+    private DynamicCallBackInfo findDynamicCallbackInfoForView(PsiDeclarationStatement statement, String viewId) {
+        DynamicCallBackInfo result = null;
+        PsiDeclarationStatementImpl declarationStatement = (PsiDeclarationStatementImpl) statement;
+        try {
+            PsiElement[] children = declarationStatement.getChildren();
+            if (children.length > 0 && children[0] instanceof PsiLocalVariableImpl) {
+                PsiLocalVariableImpl variable = (PsiLocalVariableImpl) children[0];
+                PsiExpression expression = variable.getInitializer();
+                if (expression instanceof PsiTypeCastExpressionImpl) {
+                    PsiTypeCastExpressionImpl typeCastExpression = (PsiTypeCastExpressionImpl) expression;
+                    PsiExpression operand = typeCastExpression.getOperand();
+                    if (operand instanceof PsiMethodCallExpressionImpl) {
+                        if (hasAccessToView((PsiMethodCallExpression) operand, viewId)) {
+                           // String viewVariable = variable.getText();
+                            PsiElement[] siblings = statement.getParent().getChildren();
+                            for(PsiElement sibling: siblings ) {
+                                Utils.showMessage("\t\t\t>>" +sibling.getText());
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private DynamicCallBackInfo findDynamicCallbackInfoForView(PsiAssignmentExpression assignmentExpression, String viewId) {
+        DynamicCallBackInfo result = null;
+        PsiExpression rightExpression = assignmentExpression.getRExpression();
+        PsiExpression leftExpression = assignmentExpression.getLExpression();
+        if (rightExpression instanceof PsiMethodCallExpression) {
+            PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) rightExpression;
+            if (hasAccessToView(methodCallExpression, viewId)) {
+                //String viewVariable = leftExpression.getReference().getElement().getText();
+                PsiElement[] siblings = assignmentExpression.getParent().getChildren();
+                for(PsiElement sibling: siblings ) {
+                    Utils.showMessage("\t\t\t>>" +sibling.getText());
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+
+    private boolean hasAccessToView(PsiMethodCallExpression methodCallExpression, String viewId) {
+        boolean result = false;
+        String calledMethodName = methodCallExpression.getMethodExpression().getReferenceName();
+        if (calledMethodName.equals("findViewById")) {
+            PsiExpressionList arguments = methodCallExpression.getArgumentList();
+            PsiExpression firstArgument = arguments.getExpressions()[0];
+            if (firstArgument.getText().equals("R.id." + viewId)) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+}
+
+
+class DynamicCallBackInfo {
+    private String viewId;
+    private PsiMethod method;
+
+    public DynamicCallBackInfo(String viewId, PsiMethod method) {
+        this.viewId = viewId;
+        this.method = method;
+    }
+
+    public PsiMethod getMethod() {
+        return method;
+    }
+
+    public String getViewId() {
+        return viewId;
+    }
 }
